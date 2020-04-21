c51f90ec25f47c40dbe91b9e00b5ac1156d44810
Bharat Ramnani <bharatramnani94@gmail.com>
2020-04-22 00:00:39 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+let scrape = async () => {
+  // Actual Scraping goes Here...
+
+  return Math.random();
+
+  // Return a value
+};
+
+const result = await scrape();
+console.log(result);
