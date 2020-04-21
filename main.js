c1faf13d648e5e03906b1f11b41d4d1badffb336
Bharat Ramnani <bharatramnani94@gmail.com>
2020-04-22 00:12:09 +0530

    Scrape chapters

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
-// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 
-let scrape = async () => {
-  // Actual Scraping goes Here...
+const CONFIG = {
+  useHeadlessBrowser: true,
+}
+const DATA = {
+  urlOfIndexPage: "https://seanwes.com/overlapbook/",
 
-  return Math.random();
+}
 
-  // Return a value
+const openBrowser = async() => {
+  const browser = await puppeteer.launch({ headless: CONFIG.useHeadlessBrowser });
+  return browser;
+}
+
+const scrape = async () => {
+
+  const browser = await openBrowser();
+  const page = await browser.newPage();
+
+  await page.goto(DATA.urlOfIndexPage);
+  await page.waitFor(1000);
+
+  const result = await page.evaluate(() => {
+    const chapters = [...document.querySelectorAll('.post-article ul ul li a')].map(anchorTag => {
+      const title = anchorTag.innerText;
+      const url = anchorTag.href;
+      return { title, url };
+    });
+    return chapters;
+  });
+
+  browser.close();
+  return result;
+
 };
 
 (async() => {
